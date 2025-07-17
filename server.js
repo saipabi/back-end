ac9b92076eda69ab02c6c92cd4503b4a5b66698f
saipabi <saipabi123@gmail.com>
2025-07-17 13:30:04 +0530

    done....

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
-
+require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-mongoose.connect("mongodb+srv://leader:2e79vqlQuHAPVDkd@cluster0.br0zagv.mongodb.net/leaderboard?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
 app.use('/', userRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
